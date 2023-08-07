ba77e5d8ad8cfdd9b855402d14911151b36db924
Cem Karadeniz <ckaradeniz@alpineitw.com>
2023-08-07 09:29:13 -0400

    Added environment condition for cache

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -6,22 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"), opts => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING"), opts =>
+    {
         opts.EnableRetryOnFailure();
     });
     options.EnableDetailedErrors();
     options.EnableSensitiveDataLogging();
 });
 
-// For local
-// builder.Services.AddDistributedMemoryCache();
-
-// For deployment
-builder.Services.AddStackExchangeRedisCache(options =>
+if (builder.Environment.IsDevelopment())
 {
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
-    options.InstanceName = "SampleInstance";
-});
+    // For local
+    builder.Services.AddDistributedMemoryCache();
+}
+else
+{
+    // For Azure App deployment
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+        options.InstanceName = "SampleInstance";
+    });
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
